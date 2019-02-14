2be20e331ab52ac0cf4da21b861c79ea12e88156
Asma'a M Alqrinawi <ward8175@gmail.com>
2019-02-14 13:28:34 +0200

    website
    heyu website

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    var scrollTop =0;
+    $(window).scroll(function(){
+        scrollTop = $(window).scrollTop();
+        if (scrollTop > 100){
+            $('.navbar').addClass ('scrollNav');
+			$('.backToTop').addClass ('visible');
+			
+        }else if (scrollTop < 100){
+            $('.navbar').removeClass('scrollNav');
+			$('.backToTop').removeClass ('visible');
+        };
+    });
+	$('.backToTop').click (function(){
+		$('body , html').animate({
+			scrollTop:0
+		},800);
+	});
+});
